413b03b2600e92350e221cf3197090bb841c3ed3
JACINTO WONG <jacinto.wong@cbc.ca>
2020-04-01 17:27:27 -0400

    add to column functionality

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
-
+// Add/Save Btns, Items for every Column
 const saveItemBtns = document.querySelectorAll('.solid');
 const addContainers = document.querySelectorAll('.add-container');
-const addBtns = document.querySelectorAll('.add-btn:not(.solid)')
-const addBacklog = document.getElementById('add-backlog');
-const addProgress = document.getElementById('add-progress');
-const addComplete = document.getElementById('add-complete');
-const addOnHold = document.getElementById('add-on-hold');
+const addBtns = document.querySelectorAll('.add-btn:not(.solid)');
+const addItems = document.querySelectorAll('.add-item');
 
+// Column Lists
+const backlogList = document.getElementById('backlog-list');
+const progressList = document.getElementById('progress-list');
+const completeList = document.getElementById('complete-list');
+const onHoldList = document.getElementById('on-hold-list');
+
+let itemText = '';
+
+// Show Add Item Input Box
 function showInputBox(column) {
-    console.log(column);
+    // console.log(column);
     addBtns[column].style.visibility = 'hidden';
     saveItemBtns[column].style.display = 'flex';
     addContainers[column].style.display = 'flex';
 }
 
+// Hide Item Input Box
 function hideInputBox(column) {
     addBtns[column].style.visibility = 'visible';
     saveItemBtns[column].style.display = 'none';
     addContainers[column].style.display = 'none';
+    addtoColumn(column);
+}
+
+// Add to Column List
+function addtoColumn(column) {
+    console.log(column);
+    if (column == 0) {
+        itemText = addItems[column].innerText;
+        console.log(itemText);
+        backlogList.innerHTML += `<li class="drag-item" contenteditable="true">${itemText}</li>`;
+    }
+    if (column == 1) {
+        itemText = addItems[column].innerText;
+        console.log(itemText);
+        progressList.innerHTML += `<li class="drag-item" contenteditable="true">${itemText}</li>`;
+    }
+    if (column == 2) {
+        itemText = addItems[column].innerText;
+        console.log(itemText);
+        completeList.innerHTML += `<li class="drag-item" contenteditable="true">${itemText}</li>`;
+    }
+    if (column == 3) {
+        itemText = addItems[column].innerText;
+        console.log(itemText);
+        onHoldList.innerHTML += `<li class="drag-item" contenteditable="true">${itemText}</li>`;
+    }
 }
