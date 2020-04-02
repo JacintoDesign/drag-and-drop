b444fb0d37653633d858d5a1bf53a3ada8bb38a7
JACINTO WONG <jacinto.wong@cbc.ca>
2020-04-02 13:12:11 -0400

    delete functionality

--- script.js
+++ script.js
@@ -21,58 +21,89 @@ let progressListArray = [ "Lorem ipsum dolor sit amet consectetur adipisicing el
 let completeListArray = [ "Being cool", "Getting stuff done"];
 let onHoldListArray = [ "Being uncool" ];
 
-// Update Item
+// Update Item - Delete if necessary, or update Array value
 function updateItem(id, column) {
-    console.log('testing that', id, column);
+    // console.log('testing that', id, column);
     if (column == 0) {
-        backlogListArray[id] = backlogList.children[id].innerText;
+        if (backlogList.children[id] == undefined || backlogList.children[id].innerText == '') {
+            delete backlogListArray[id];
+        } else {
+            backlogListArray[id] = backlogList.children[id].innerText;
+        }
     }
     if (column == 1) {
-        progressListArray[id] = progressList.children[id].innerText;
+        if (progressList.children[id] == undefined || progressList.children[id].innerText == '') {
+            delete progressListArray[id];
+        } else {
+            progressListArray[id] = progressList.children[id].innerText;
+        }
     }
     if (column == 2) {
-        completeListArray[id] = completeList.children[id].innerText;
+        if (completeList.children[id] == undefined || completeList.children[id].innerText == '') {
+            delete completeListArray[id];
+        } else {
+            completeListArray[id] = completeList.children[id].innerText;
+        }
     }
     if (column == 3) {
-        onHoldListArray[id] = onHoldList.children[id].innerText;
+        if (onHoldList.children[id] == undefined || onHoldList.children[id].innerText == '') {
+            delete onHoldListArray[id];
+        } else {
+            onHoldListArray[id] = onHoldList.children[id].innerText;
+        }
     }
     updateArrays(column);
 }
 
-// Update Arrays
+// Update Arrays - Reset container HTML, loop through and add back items, exclude items with no value
 function updateArrays(column) {
     if (column == 0 || !updatedOnLoad) {
-        console.log('updateonLoad', updatedOnLoad);
+        //console.log('updateonLoad', updatedOnLoad);
         backlogList.innerHTML = '';
-        console.log('backlog empty', backlogList.innerHTML);
         for (i = 0; i < backlogListArray.length; i++) {
-            backlogList.innerHTML += `
-            <li id="${[i]}" class="drag-item" contenteditable="true" onfocusout="updateItem(this.id, 0)">${backlogListArray[i]}</li>
-            `;
+            if (backlogListArray[i] == undefined) {
+                console.log('no value');
+            } else {
+                backlogList.innerHTML += `
+                <li id="${[i]}" class="drag-item" contenteditable="true" onfocusout="updateItem(this.id, ${column})">${backlogListArray[i]}</li>
+                `;
+            }
         }
     }
     if (column == 1 || !updatedOnLoad) {
         progressList.innerHTML = '';
         for (i = 0; i < progressListArray.length; i++) {
-            progressList.innerHTML += `
-            <li id="${[i]}" class="drag-item" contenteditable="true" onfocusout="updateItem(this.id, 1)">${progressListArray[i]}</li>
-            `;
+            if (progressListArray[i] == undefined) {
+                console.log('no value');
+            } else {
+                progressList.innerHTML += `
+                <li id="${[i]}" class="drag-item" contenteditable="true" onfocusout="updateItem(this.id, ${column})">${progressListArray[i]}</li>
+                `;
+            }
         }
     }
     if (column == 2 || !updatedOnLoad) {
         completeList.innerHTML = '';
         for (i = 0; i < completeListArray.length; i++) {
-            completeList.innerHTML += `
-            <li id="${[i]}" class="drag-item" contenteditable="true" onfocusout="updateItem(this.id, 2)">${completeListArray[i]}</li>
-            `;
+            if (completeListArray[i] == undefined) {
+                console.log('no value');
+            } else {
+                completeList.innerHTML += `
+                <li id="${[i]}" class="drag-item" contenteditable="true" onfocusout="updateItem(this.id, ${column})">${completeListArray[i]}</li>
+                `;
+            }
         }
     }
     if (column == 3 || !updatedOnLoad) {
         onHoldList.innerHTML = '';
         for (i = 0; i < onHoldListArray.length; i++) {
-            onHoldList.innerHTML += `
-            <li id="${[i]}" class="drag-item" contenteditable="true" onfocusout="updateItem(this.id, 3)">${onHoldListArray[i]}</li>
-            `;
+            if (onHoldListArray[i] == undefined) {
+                console.log('no value');
+            } else {
+                onHoldList.innerHTML += `
+                <li id="${[i]}" class="drag-item" contenteditable="true" onfocusout="updateItem(this.id, ${column})">${onHoldListArray[i]}</li>
+                `;
+            }
         }
     }
     updatedOnLoad = true;
@@ -98,28 +129,32 @@ function hideInputBox(column) {
     addtoColumn(column);
 }
 
-// Add to Column List
+// Add to Column List, Then Clear Text
 function addtoColumn(column) {
-    console.log(column);
+    //console.log(column);
     if (column == 0) {
         itemText = addItems[column].innerText;
         console.log(itemText);
         backlogListArray.push(itemText);
+        addItems[column].innerText = '';
     }
     if (column == 1) {
         itemText = addItems[column].innerText;
         console.log(itemText);
         progressListArray.push(itemText);
+        addItems[column].innerText = '';
     }
     if (column == 2) {
         itemText = addItems[column].innerText;
         console.log(itemText);
         completeListArray.push(itemText);
+        addItems[column].innerText = '';
     }
     if (column == 3) {
         itemText = addItems[column].innerText;
         console.log(itemText);
         onHoldListArray.push(itemText);
+        addItems[column].innerText = '';
     }
     updateArrays(column);
 }
